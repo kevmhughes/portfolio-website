3e2c9d6d4e6cc3f289947b41e6ed1bbf47382e14
kevmhughes <kevhughes24@hotmail.com>
2025-06-20 02:14:46 +0200

    feat: redesign Projects page and update Home page intro section

--- docs/js/index.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to rotate the image on scroll
-window.addEventListener("scroll", () => {
-  const image = document.getElementById("rotateImage");
-  const scrollPosition = window.scrollY; // Get the scroll position
-  const rotationAngle = scrollPosition % 1240; // Limit the rotation to 360 degrees
-
-  // Apply rotation to the image
-  image.style.transform = `rotate(${rotationAngle}deg)`;
-});
