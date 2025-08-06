a76b98b8f0cc8e8b1957f0a637c53c11d376f877
kevmhughes <kevhughes24@hotmail.com>
2025-08-06 17:21:28 +0200

    style: animated gradient background and improve mobile nav
    Introduces an animated gradient background to the home page and updates navigation bar classes for consistency.

--- docs/js/hamburger-menu.js
+++ docs/js/hamburger-menu.js
@@ -1,18 +1,26 @@
 const burger = document.getElementById('burger');
-const navItems = document.getElementById('nav-items-mobile'); 
+const navItems = document.getElementById('nav-items-mobile');
 const html = document.documentElement;
 const body = document.body;
+const mobileNav = document.getElementById('nav-bar-mobile');
 
 burger.addEventListener('click', () => {
   const isActive = navItems.classList.toggle('active');
   burger.classList.toggle('active');
 
-  // Lock or unlock scrolling
+
   if (isActive) {
+    // Lock scrolling
     html.classList.add('no-scroll');
     body.classList.add('no-scroll');
+    // Add white background to mobile nav
+    mobileNav.classList.add('white-bg');
+
   } else {
+    // Unlock scrolling
     html.classList.remove('no-scroll');
     body.classList.remove('no-scroll');
+    // Remove white background to mobile nav
+    mobileNav.classList.remove('white-bg');
   }
 });
