496a3aca6588bb411884bace69925d8475730aca
kevmhughes <kevhughes24@hotmail.com>
2025-08-19 01:03:53 +0200

    fix: update asset paths and add lazy loading script
    Changed favicon and asset paths in HTML files from absolute to relative for better portability. Added a new lazy-loading.js script to optimize image loading based on device width. Updated preload-images.js to use relative paths. Minor CSS animation delay adjustment for improved UI responsiveness.

--- docs/js/preload-images.js
+++ docs/js/preload-images.js
@@ -1,10 +1,10 @@
 const projectImages = [
-  "./assets/mock-ups/andaluciabirdland-laptop.jpg",
-  "./assets/mock-ups/livedocs-laptop.jpg",
-  "./assets/mock-ups/mello-laptop.jpg",
-  "./assets/mock-ups/yariga-laptop.jpg",
-  "./assets/mock-ups/flowerheads-laptop.jpg",
-  "./assets/mock-ups/krypt-laptop.jpg",
+  "assets/mock-ups/andaluciabirdland-laptop.jpg",
+  "assets/mock-ups/livedocs-laptop.jpg",
+  "assets/mock-ups/mello-laptop.jpg",
+  "assets/mock-ups/yariga-laptop.jpg",
+  "assets/mock-ups/flowerheads-laptop.jpg",
+  "assets/mock-ups/krypt-laptop.jpg",
 ];
 
 projectImages.forEach((src) => {
